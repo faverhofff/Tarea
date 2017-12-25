ac47943d263c5155900757979428bf449606e817
frank <faverhofff@gmail.com>
2017-12-25 00:43:10 -0500

    Entity model for data base.

---
+++ ApiTarea/Models/Page.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTarea.Models
+{
+    public class Page
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        public string UrlSource { get; set; }
+    }
+}
