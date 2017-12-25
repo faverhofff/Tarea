6d5952850b0688f72d1b4df90848228b4b9f93e8
frank <faverhofff@gmail.com>
2017-12-25 00:44:28 -0500

    Singleton for MongoDb class instance.

---
+++ ApiTarea/Services/MongoDBInstance.cs
@@ -0,0 +1,22 @@
+﻿using ApiTarea.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiTarea.Services
+{
+    static class MongoDBInstance
+    {
+        private readonly static MongoDbContext _instance = new MongoDbContext("192.168.56.102");
+
+        public static MongoDbContext getInstance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+    }
+}
