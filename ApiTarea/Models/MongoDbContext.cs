0f8e9df98887bd90f6da1d4af099d8c739172f81
frank <faverhofff@gmail.com>
2017-12-25 00:42:20 -0500

    MongoDB class connection.

---
+++ ApiTarea/Models/MongoDbContext.cs
@@ -0,0 +1,51 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using ApiTarea.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ApiTarea.Models
+{
+    public class MongoDbContext
+    {
+        private MongoClient _Connection;
+
+        private IMongoDatabase _Database;
+
+        public MongoDbContext()
+        {
+            _Connection = new MongoClient();
+            _Database = _Connection.GetDatabase("Pages");
+        }
+
+        public MongoDbContext(string url)
+        {
+            _Connection = new MongoClient("mongodb://" + url);
+            _Database = _Connection.GetDatabase("Pages");
+        }
+
+        public async Task Insert(Page data)
+        {
+            var collection = _Database.GetCollection<Page>("Pages");
+            await collection.InsertOneAsync(data);
+        }
+
+        public void Insert(List<Page> data)
+        {
+            var collection = _Database.GetCollection<Page>("Pages");
+            collection.InsertMany(data);
+        }
+
+        public List<Page> SelectByWord(string Word)
+        {
+            var filter = Builders<Page>.Filter.Regex("Content", new BsonRegularExpression(Word));
+            return _Database.GetCollection<Page>("Pages").Find(filter).ToList<Page>();
+        }
+
+        public void Remove()
+        {
+            _Database.GetCollection<Page>("Pages").DeleteManyAsync(new BsonDocument());
+        }
+    }
+
+}
