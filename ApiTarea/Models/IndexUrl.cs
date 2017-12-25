0bf90f99ea8d82447541a51f4f1aa8104b17cb60
frank <faverhofff@gmail.com>
2017-12-25 00:46:17 -0500

    Class for web service head reception.

---
+++ ApiTarea/Models/IndexUrl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiTarea.Models
+{
+    public class IndexUrl
+    {
+        [Required]
+        public string Url { get; set; }
+    }
+}
+
