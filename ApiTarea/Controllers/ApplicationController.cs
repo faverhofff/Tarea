3182dc53242bb468b650b224582b1298f4a7bffa
frank <faverhofff@gmail.com>
2017-12-25 00:46:35 -0500

    Web Service Controller.

---
+++ ApiTarea/Controllers/ApplicationController.cs
@@ -0,0 +1,56 @@
+﻿using System.Web.Http;
+using ApiTarea.Services;
+using ApiTarea.Models;
+using System.Collections.Generic;
+using System.Web.Http.Results;
+
+namespace ApiTarea.Controllers
+{
+    [RoutePrefix("api")]
+    public class ApplicationController : ApiController
+    {
+        protected Aplication _Service { get; set; }
+
+        public ApplicationController()
+        {
+            _Service = new Aplication();                
+        }
+
+        [HttpGet]
+        // [HttpOptions]
+        [Route("search")]
+        public IHttpActionResult Search(Search Form)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Json(_Service.Search(Form.Word));
+        }
+
+        [HttpPost]
+        [HttpGet]
+        [Route("index")]
+        public IHttpActionResult IndexPage([FromBody] IndexUrl Site)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            IndexUrl n = new IndexUrl();
+            n.Url = "http://192.168.56.101:83/login";
+            return Json(_Service.Scrap(new List<string>() { n.Url }));
+        }
+
+        [HttpGet]
+        [Route("clear")]
+        public IHttpActionResult ClearPage([FromBody] IndexUrl Site)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _Service.RemoveIndexContent();
+
+            return Json(new List<bool>() { true });
+        }
+
+
+    }
+}
