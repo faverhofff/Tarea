b27fa3aed171f54230ecc48d2dc642c6bfbfabb6
frank <faverhofff@gmail.com>
2017-12-25 00:45:30 -0500

    WebScrap functionality.

---
+++ ApiTarea/Services/UrlParser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+// using System.Web;
+
+namespace ApiTarea.Services
+{
+  /// <summary>
+  /// Summary description for UrlParser
+  /// </summary>
+  public class UrlParser
+  {
+        public UrlParser()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static bool checkURLFormat(string url)
+        {
+            Uri UriResult;
+
+            return Uri.TryCreate(url, UriKind.Absolute, out UriResult) && (UriResult.Scheme == Uri.UriSchemeHttp || UriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static bool checkURLStatus(string url)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                    using (client.OpenRead(url))
+                        return true;
+            } 
+            catch
+            {
+                return false;
+            }
+        }
+  }
+}
---
+++ ApiTarea/Services/WebScrap.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using ApiTarea.Models;
+using static ApiTarea.Services.LinkFinder;
+
+namespace ApiTarea.Services
+{
+    /// <summary>
+    /// Descripción breve de WebScrap
+    /// </summary>
+    /// 
+    public class WebScrap 
+    {
+        public List<string> MainURL { get; private set; } = new List<string>();
+
+        public List<string> nextUrls { get; private set; } = new List<string>();
+
+        private WebClient _Client { get; set; } = new WebClient();
+
+        private string _Buffer = "";
+
+        protected WebScrap() { }
+
+        public WebScrap(string url)
+        {
+            MainURL.Add(url);
+        }
+
+        public WebScrap(List<string> url)
+        {
+            MainURL.AddRange(url);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        protected Page scrapPage( string url )
+        {
+            _Buffer = _Client.DownloadString(url);
+
+            foreach (LinkItem link in LinkFinder.Find(_Buffer)) 
+                nextUrls.Add(link.Href);
+
+            Page row = new Page();
+            row.Id = Guid.NewGuid();
+            row.Title = ""; //buscar expresion regular de TITLE
+            row.Content = _Buffer; // convertir html to text
+            row.Url = url;
+
+            return row;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Page> scrapPages()
+        {
+            List<Page> _result = new List<Page>();
+
+            foreach (string url in MainURL)
+                _result.Add( this.scrapPage(url) );
+
+            return _result;
+        }
+
+ 
+    }
+}
