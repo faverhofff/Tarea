a277138d0a51ff0096469cae732e00ce1938a19c
frank <faverhofff@gmail.com>
2017-12-26 13:11:30 -0500

    Add last file.

---
+++ ApiTarea/Models/IndexResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiTarea.Models
+{
+    public class IndexResult
+    {
+        public int pageIndexTotal;
+
+        public int pageWordsCount;
+    }
+}
